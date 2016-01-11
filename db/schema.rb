51d06ffdd0d8062cd2d540a5530d3f01025e6ce7
munisystem <munisystem@gmail.com>
2016-01-11 15:30:15 +0900

    Add comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111031850) do
+ActiveRecord::Schema.define(version: 20160111062838) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "video"
+    t.string   "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "videos", force: :cascade do |t|
     t.string   "video"
