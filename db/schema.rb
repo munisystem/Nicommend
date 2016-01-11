cf5637761ceca675ab91c7a51f36de07c067940a
munisystem <munisystem@gmail.com>
2016-01-11 15:31:15 +0900

    Add word model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160111062838) do
+ActiveRecord::Schema.define(version: 20160111063104) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "video"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20160111062838) do
 
   create_table "videos", force: :cascade do |t|
     t.string   "video"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.string   "video"
+    t.string   "word"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
